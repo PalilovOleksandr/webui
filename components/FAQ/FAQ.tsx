bb646a38f2726d0d4c3e3568a09b9ac8927ac496
Oleksandr Palilov <opalilovit@gmail.com>
2025-11-04 23:57:38 +0200

    added FAQ component

--- components/FAQ/FAQ.tsx
+++ components/FAQ/FAQ.tsx
@@ -1,26 +1,62 @@
 import css from './FAQ.module.css';
-import { Accordion, AccordionItem } from '@szhsin/react-accordion';
-import alimonyServiceImg from '@/public/images/AlimonyService.jpg';
-
 import Image from 'next/image';
+import FAQImg from '@/public/images/AlimonyService.jpg';
+import { IoIosArrowDown } from 'react-icons/io';
+import { RiArrowDownDoubleFill } from 'react-icons/ri';
 
-const FAQ = () => {
+const Review = () => {
+  const faqItems = [
+    'Які потрібні документи для розірвання шлюбу чарез суд?',
+    'Яка вартість судового збору?',
+    'Скільки триває процес розірвання шлюбу через суд?',
+    'Які є варіанти розірвання шлюбу в Україні?',
+    'Чи потрібно відвідувати суд?',
+    'Чи потрібна згода іншого з подружжя?',
+  ];
+
   return (
-    <section>
-      <h3></h3>
-      <div>
-        <div className={css.imageWrapper}>
-          <Image
-            src={alimonyServiceImg}
-            alt="Alimony service photo"
-            width={244}
-            height={310}
-            className={css.image}
-          />
+    <div className={css.faqContainer}>
+      <div className={css.contentWrapper}>
+        <div className={css.titleSection}>
+          <h3 className={css.mainTitle}>
+            <span className={css.titleWordLeft}>ПОШИРЕНІ</span>
+            <span className={css.titleWordRight}>ЗАПИТАННЯ</span>
+          </h3>
+        </div>
+        <div className={css.mainContent}>
+          <div className={css.imageBlock}>
+            <span className={css.verticalLine}></span>
+            <div className={css.imageWrapper}>
+              <Image
+                src={FAQImg}
+                alt="Люди за столом, обговорюють документи"
+                width={244}
+                height={310}
+                className={css.image}
+              />
+            </div>
+          </div>
+          <div className={css.faqBlock}>
+            {faqItems.map((item, index) => (
+              <div key={index} className={css.faqItem}>
+                <p className={css.faqQuestion}>{item}</p>
+                <span className={css.arrowIcon}>
+                  <IoIosArrowDown className={css.arrowIcon} />
+                </span>
+              </div>
+            ))}
+            <button className={css.moreButton}>
+              БІЛЬШЕ
+              <span className={css.arrowDown}>
+                <RiArrowDownDoubleFill />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
-    </section>
+      <div className={css.bottomBar}></div>
+    </div>
   );
 };
 
-export default FAQ;
+export default Review;
