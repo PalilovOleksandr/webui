c140bfe818da5a3ddaf2d9bec397f83962c79647
Oleksandr Palilov <opalilovit@gmail.com>
2025-11-10 00:34:39 +0200

    fix build error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -46,7 +46,7 @@ export default async function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  params: { locale: string };
 }>) {
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
